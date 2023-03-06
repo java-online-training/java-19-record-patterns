0604eb7934a3a0fae8094af446de6ffbd6e37a7e
superernie77 <superernie77@gmail.com>
2023-03-06 16:10:32 +0100

    final version

--- src/main/java/jot/PatternMatchingExamples.java
+++ src/main/java/jot/PatternMatchingExamples.java
@@ -1,5 +1,7 @@
 package jot;
 
+import java.util.*;
+
 public class PatternMatchingExamples {
 	public static void main(String[] args) {
 		Zoo zoo = new Zoo(new Monkey("Jake", 5), new Donkey("Bob", 3));
@@ -11,6 +13,16 @@ public class PatternMatchingExamples {
 		System.out.println("With new pattern matching :");
 		
 		animalAge(zoo);		
+	}
+	
+	/**
+	 * Version with new pattern matching feature
+	 */
+	public static void animalAge(Object o) {
+		if (o instanceof Zoo (Monkey(String mName,int mAge) m, Donkey(String dName,int dAge) d )) {
+			System.out.println(mName + " the monkey is "+ mAge + " years old." );
+			System.out.println(dName + " the donkey is "+ dAge + " years old." );
+		}
 	}
 	
 	/**
@@ -27,19 +39,10 @@ public class PatternMatchingExamples {
 			System.out.println(dName + " the donkey is "+ dAge + " years old." );
 		}
 	}
-	
-	/**
-	 * Version with new pattern matching feature
-	 */
-	public static void animalAge(Object o) {
-		if (o instanceof Zoo (Monkey(String mName,int mAge) m, Donkey(String dName,int dAge) d )) {
-			System.out.println(mName + " the monkey is "+ mAge + " years old." );
-			System.out.println(dName + " the donkey is "+ dAge + " years old." );
-		}
-	}
-	
-	
 }
+
 record Monkey(String name, int age) {};
 record Donkey(String name, int age) {};
 record Zoo(Monkey monkey, Donkey donkey){};
+
+
